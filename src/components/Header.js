1d8f3eb4fadbe985f264377c5cbb0593edf04a6a
ramazankarisan <rkarisan25@gmail.com>
2022-01-20 23:51:14 +0100

    header icons added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { BookmarkAltIcon, HomeIcon, LightBulbIcon, LoginIcon, TrendingDownIcon, TrendingUpIcon } from "@heroicons/react/outline"
+import { BookmarkAltIcon, HomeIcon, LightBulbIcon, LoginIcon, TrendingUpIcon } from "@heroicons/react/outline"
 
 const Header = () => {
 
   return <>
-    <div>
-      <HomeIcon className="w-8 text-white" />
-      <TrendingUpIcon className="w-8 text-white" />
-      <BookmarkAltIcon className="w-8 text-white" />
-      <LightBulbIcon className="w-8 text-white" />
-      <LoginIcon className="w-8 text-white" />
+    <div className='flex justify-center space-x-5 my-4  '>
+
+      <div className='flex flex-col justify-center items-center space-y-2 group cursor-pointer '>
+        <HomeIcon className="w-8 text-white group-hover:animate-bounce" />
+        <p className='text-white text-lg font-bold tracking-wider opacity-0 group-hover:opacity-100 '>Home</p>
+      </div>
+
+      <div className='flex flex-col justify-center items-center space-y-2 group cursor-pointer '>
+        <TrendingUpIcon className="w-8 text-white group-hover:animate-bounce " />
+        <p className='text-white text-lg font-bold tracking-wider opacity-0 group-hover:opacity-100'>Trending</p>
+      </div>
+
+      <div className='flex flex-col justify-center items-center space-y-2 group cursor-pointer '>
+        <BookmarkAltIcon className="w-8 text-white group-hover:animate-bounce" />
+        <p className='text-white text-lg font-bold tracking-wider opacity-0 group-hover:opacity-100'> Collections</p>
+      </div>
+
+      <div className='flex flex-col justify-center items-center space-y-2 group cursor-pointer '>
+        <LightBulbIcon className="w-8 text-white group-hover:animate-bounce" />
+        <p className='text-white text-lg font-bold tracking-wider opacity-0 group-hover:opacity-100'>Random</p>
+      </div>
+
+      <div className='flex flex-col justify-center items-center space-y-2 group cursor-pointer '>
+        <LoginIcon className="w-8 text-white group-hover:animate-bounce" />
+        <p className='text-white text-lg font-bold tracking-wider opacity-0 group-hover:opacity-100'>Login</p>
+      </div>
 
     </div>
   </>;
