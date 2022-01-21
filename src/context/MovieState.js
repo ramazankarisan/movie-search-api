d2d04c59c2847a82f14fbfc0188247bc1d3620d5
ramazankarisan <rkarisan25@gmail.com>
2022-01-21 23:52:11 +0100

    movieContext created

---
+++ src/context/MovieState.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieContext from './MovieContext';
+
+const MovieState = () => {
+
+
+  return (
+    <MovieContext.Provider value={{}}>
+
+    </MovieContext.Provider>
+  )
+}
+
+export default MovieState;
