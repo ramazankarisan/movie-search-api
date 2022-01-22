93df505973a8a5a78b1f278383b611fab688f64f
ramazankarisan <rkarisan25@gmail.com>
2022-01-22 15:41:10 +0100

    navbarTrend created

--- src/components/NavbarTrends.js
+++ src/components/NavbarTrends.js
@@ -5,14 +5,16 @@ import { requests } from "../API/requests"
 const NavbarTrends = () => {
   const navigate = useNavigate()
   return <>
-
-    {Object.entries(requests).map(([key, { title, url }]) => {
-      return (
-        <h2 key={title}
-          onClick={() => navigate(`/${key}`)}
-        > {title}</h2>
-      )
-    })}
+    <div className='flex px-10 sm:px-20 whitespace-nowrap space-x-10 sm:space-x-20 text-xl text-white  overflow-x-scroll scrollbar-hide sm:justify-evenly scroll-horizon '>
+      {Object.entries(requests).map(([key, { title, url }]) => {
+        return (
+          <h2 key={title}
+            onClick={() => navigate(`/${key}`)}
+            className='cursor-pointer transition duration-100 transform hover:scale-125 active:text-red-500'
+          > {title}</h2>
+        )
+      })}
+    </div>
   </>;
 };
 
